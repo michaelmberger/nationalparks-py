3809ac004b35db87663003e58fdcf7967dcf76ee
michaelmberger <michaelmberger@gmail.com>
2017-08-09 14:51:22 -0300

    second try

--- wsgi.py
+++ wsgi.py
@@ -32,7 +32,7 @@ api.add_resource(HealthCheck, '/ws/healthz/')
 class Info(Resource):
     description = {
         'id': 'nationalparks-py',
-        'displayName': 'Some Other Display Name',
+        'displayName': 'Some Other Display',
         'type': 'cluster',
         'center': {'latitude': '47.039304', 'longitude': '14.505178'},
         'zoom': 4
